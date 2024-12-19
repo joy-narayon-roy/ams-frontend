38183fcb2497f301ec6e7a238f1310f1ea19af01
joy-narayon-roy <joyroy261216@gmail.com>
2024-12-19 21:32:37 +0600

    rebuild

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,6 @@ import { AuthProvider } from "./contexts/AuthContext/index";
 import Header from "./components/Header";
 import AppRoutes from "./AppRoutes";
 
-// function Main() {
-//   // const authContest = useAuthContext();
-//   // console.log(authContest);
-//   return (
-//     <h1 className="text-3xl text-center mt-5">Account Management System</h1>
-//   );
-// }
-
 function App() {
   return (
     <>
@@ -18,7 +10,6 @@ function App() {
         <AuthProvider>
           <Header />
           <AppRoutes />
-          {/* <Main /> */}
         </AuthProvider>
       </BrowserRouter>
     </>
